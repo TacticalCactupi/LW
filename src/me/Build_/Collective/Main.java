402722418494959cc256bd6892cea1b91b365d9f
TacticalCactupi <clk.musicman@gmail.com>
2014-03-24 11:08:50 -0400

    UPDATE! [Almost]
    Finished logging, still have to finish commands and actually showing
    locations..... somehow. You'll see, eventually!

--- src/me/Build_/Collective/Main.java
+++ src/me/Build_/Collective/Main.java
@@ -2,14 +2,18 @@ package me.Build_.Collective;
 import code.husky.*;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.sql.StatementEvent;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -37,6 +41,7 @@ public class Main extends JavaPlugin {
     //-----> Disable Setup <-----\\
 	@Override
 	public void onDisable() {
+		Connection c = null;
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info(pdfFile.getName()
 				+ " was successfully disabled. Goodbye!");
@@ -47,8 +52,18 @@ public class Main extends JavaPlugin {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onEnable() {
+		
+		//-----> Setup Tables <-----\\
 		c = MySQL.openConnection();
-		//Hashed, update, open.
+		try {
+	    	Statement tableSetup;
+	    	tableSetup = MySQL.openConnection().createStatement();;
+			tableSetup.executeQuery("CREATE TABLE IF NOT EXISTS `LW`");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		//-----> Main Timer <-----\\
 		this.getServer().getScheduler()
 				.scheduleAsyncRepeatingTask(this, new Runnable() {
 					public void run() {
@@ -56,6 +71,26 @@ public class Main extends JavaPlugin {
 							number--;
 						} else {
 							int logCount = Bukkit.getServer().getOnlinePlayers().length;
+						    
+							//-----> Main Logger <-----\\
+						    PreparedStatement log;
+							try {
+								log = MySQL.openConnection().prepareStatement("INSERT INTO LW (`ign`, `world`, `x`, `y`, `z`, `timestamp`) VALUES (?, ?, ?, ?, ?,);");
+								for (Player p : Bukkit.getOnlinePlayers())
+							    {
+							      Location loc = p.getLocation();
+							      log.setString(1, p.getName());
+							      log.setString(2, loc.getWorld().getName());
+							      log.setString(3, String.valueOf(loc.getX()));
+							      log.setString(4, String.valueOf(loc.getY()));
+							      log.setString(5, String.valueOf(loc.getZ()));
+							      log.execute();
+							    }
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}
+							
+							//-----> Notifications <-----\\
 							if(console = true){
 								logger.info("Location Watchdog has just logged " + logCount + " player locations.");
 							}
@@ -63,6 +98,8 @@ public class Main extends JavaPlugin {
 						}
 					}
 				}, 0L, 20L);
+		
+		//-----> Main Enabler <-----\\
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion()
 				+ " was successfully enabled!");
@@ -74,7 +111,12 @@ public class Main extends JavaPlugin {
 			String commandLabel, String[] args) {
 		Player run = (Player) sender;
 		if(commandLabel.equalsIgnoreCase("lw")){
-			run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Only current command: " + ChatColor.DARK_RED + " /lw timer");
+			run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "-----" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Location Watchdog " + "[" + ChatColor.GREEN + "-----" + ChatColor.GOLD + "]");
+			run.sendMessage(ChatColor.YELLOW + " Location Watchdog is a plugin by Build_ that ");
+			run.sendMessage(ChatColor.YELLOW + " periodically logs all player locations. It can ");
+			run.sendMessage(ChatColor.YELLOW + " later show all the previous locations of a player. ");
+			run.sendMessage(ChatColor.YELLOW + " It's free on dev.bukkit.org. Thanks for using LW! ");
+			run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "-----------------------------" + ChatColor.GOLD + "]");
 	} else if (commandLabel.equalsIgnoreCase("lw timer")) {
 			int logCount = Bukkit.getServer().getOnlinePlayers().length;
 			run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Seconds before next log: " + ChatColor.DARK_RED + logCount);
