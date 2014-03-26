39c8e858c872d474df04bd52b04dbce565a4efab
TacticalCactupi <clk.musicman@gmail.com>
2014-03-26 11:31:27 -0400

    Updated [Incomplete]
    Still have to add a way to see the actual locations and revert it.
    Behind the scenes: finished plugin.yml, permissions, config.yml, and
    more.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-MySQL by -_Husky_-
-======
-Instructions: http://forums.bukkit.org/threads/tutorial-using-mysql-in-your-plugins.132309/
+Welcome to Location Watchdog!
+___________________________________________________________________
+MySQL Mainframe, by -_Husky_-, is publicly available on the forums.|
+___________________________________________________________________|
+
+For compiled downloads, please use dev.bukkit.org/bukkit-plugins/location-watchdog/.
+Feel free to fork, watch, comment, or commit to this repository! Thanks!
--- src/me/Build_/Collective/Main.java
+++ src/me/Build_/Collective/Main.java
@@ -3,10 +3,9 @@ import code.husky.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
 import javax.sql.StatementEvent;
@@ -23,19 +22,22 @@ import org.bukkit.plugin.java.JavaPlugin;
     //-----> Concluded Imports <-----\\
 @SuppressWarnings("unused")
 public class Main extends JavaPlugin {
-	//-----> Logger and Plug-in <-----\\
+	//-----> c-Logger and Plug-in <-----\\
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public static Main plugin;
+	Statement tableSetup;
+	Statement grabber;
+	int logCount = Bukkit.getServer().getOnlinePlayers().length;
 	//-----> Configuration Variables <-----\\
 	public int number = getConfig().getInt("interval");
 	public boolean console = getConfig().getBoolean("timer-notifications");
 	//-----> Grab MySQL Credentials <-----\\
 	public String dbHost = getConfig().getString("hostname", "localhost");
-	public int dbPort = getConfig().getInt("port", 3306);
+	public String dbPort = getConfig().getString("port", "3306");
 	public String dbName = getConfig().getString("database", "minecraft");
 	public String dbUser = getConfig().getString("user", "root");
 	public String dbPass = getConfig().getString("password", "password");
-    code.husky.mysql.MySQL MySQL = new code.husky.mysql.MySQL(plugin, "host.name", "port", "database", "user", "pass");
+    code.husky.mysql.MySQL MySQL = new code.husky.mysql.MySQL(plugin, dbHost, dbPort, dbName, dbUser, dbPass);
     //-----> Connection variable <-----\\
     Connection c = null;
     //-----> Disable Setup <-----\\
@@ -52,12 +54,14 @@ public class Main extends JavaPlugin {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onEnable() {
-		
+		number = getConfig().getInt("interval");
+		//-----> Config. Generator <-----\\
+		getConfig().options().copyDefaults(true);
+		saveConfig();
 		//-----> Setup Tables <-----\\
 		c = MySQL.openConnection();
 		try {
-	    	Statement tableSetup;
-	    	tableSetup = MySQL.openConnection().createStatement();;
+	    	tableSetup = MySQL.openConnection().createStatement();
 			tableSetup.executeQuery("CREATE TABLE IF NOT EXISTS `LW`");
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -70,20 +74,19 @@ public class Main extends JavaPlugin {
 						if (number != 0) {
 							number--;
 						} else {
-							int logCount = Bukkit.getServer().getOnlinePlayers().length;
 						    
 							//-----> Main Logger <-----\\
 						    PreparedStatement log;
 							try {
-								log = MySQL.openConnection().prepareStatement("INSERT INTO LW (`ign`, `world`, `x`, `y`, `z`) VALUES (?, ?, ?, ?, ?,);");
+								log = MySQL.openConnection().prepareStatement("INSERT INTO LW (`ign`, `w`, `x`, `y`, `z`) VALUES (?, ?, ?, ?, ?,);");
 								for (Player p : Bukkit.getOnlinePlayers())
 							    {
 							      Location loc = p.getLocation();
 							      log.setString(1, p.getName());
 							      log.setString(2, loc.getWorld().getName());
-							      log.setString(3, String.valueOf(loc.getX()));
-							      log.setString(4, String.valueOf(loc.getY()));
-							      log.setString(5, String.valueOf(loc.getZ()));
+							      log.setNString(3, String.valueOf(loc.getX()));
+							      log.setNString(4, String.valueOf(loc.getY()));
+							      log.setNString(5, String.valueOf(loc.getZ()));
 							      log.execute();
 							    }
 							} catch (SQLException e) {
@@ -111,16 +114,64 @@ public class Main extends JavaPlugin {
 			String commandLabel, String[] args) {
 		Player run = (Player) sender;
 		if(commandLabel.equalsIgnoreCase("lw")){
+			if(args.length == 0){
 			run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "-----" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Location Watchdog " + "[" + ChatColor.GREEN + "-----" + ChatColor.GOLD + "]");
 			run.sendMessage(ChatColor.YELLOW + " Location Watchdog is a plugin by Build_ that ");
 			run.sendMessage(ChatColor.YELLOW + " periodically logs all player locations. It can ");
 			run.sendMessage(ChatColor.YELLOW + " later show all the previous locations of a player. ");
-			run.sendMessage(ChatColor.YELLOW + " It's free on dev.bukkit.org. Thanks for using LW! ");
+			run.sendMessage(ChatColor.YELLOW + " Help: /lw help - Thanks for using LW! ");
 			run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "-----------------------------" + ChatColor.GOLD + "]");
-	} else if (commandLabel.equalsIgnoreCase("lw timer")) {
-			int logCount = Bukkit.getServer().getOnlinePlayers().length;
-			run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Seconds before next log: " + ChatColor.DARK_RED + logCount);
-		}
+			} else if (args.length == 1){
+				if (args[0] == "timer"){
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Seconds before next log: " + ChatColor.DARK_RED + number);
+				} else if (args[0] == "help"){
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Commands: ");
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.DARK_RED + " /lw - About Location Watchdog.");
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.DARK_RED + " /lw get - See LW's Bukkit page.");
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.DARK_RED + " /lw help - Shows this page.");
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.DARK_RED + " /lw timer - Shows time before next log.");
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.DARK_RED + " /lw version - Check LW's installed version.");
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.DARK_RED + " /lw check <player> <world> - Show a players locations.");
+				} else if (args[0] == "get"){
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Bukkit Dev Page:" + ChatColor.DARK_RED + "dev.bukkit.org/location-watchdog/");
+				} else if (args[0] == "version"){
+					PluginDescriptionFile pdfFile = this.getDescription();
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Installed Version: " + ChatColor.DARK_RED + pdfFile.getVersion() + " By Build_.");
+				} else if (args[0] == "check"){
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Incorrect syntax! " + ChatColor.DARK_RED + "/lw check <player> <world>");
+ 					}
+				} else if (args.length == 2){
+					run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Error: " + ChatColor.DARK_RED + "Incorrect syntax!" + ChatColor.YELLOW + " Try /lw help.");
+				} else if (args.length == 3){
+					if(args[0] == "check"){
+						if(run.hasPermission("lw.staff")){
+							String target = args[1];
+							
+							try {
+								grabber = MySQL.openConnection().createStatement();
+								ResultSet res = grabber.executeQuery("SELECT * FROM LW WHERE ign = '" + target + "';");
+								res.next();
+								if(res.getString("ign") == null){
+									run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Error: " + ChatColor.WHITE + target + ChatColor.DARK_RED + " doesn't have any logged locations!");
+								} else {
+									
+									// TODO Show player locations and make a way (command) to revert it.
+									
+								}
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}
+							
+						} else {
+							run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Error: " + ChatColor.DARK_RED + " You don't have permission!");
+						}
+					} else {
+							run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Error: " + ChatColor.DARK_RED + "Incorrect syntax!" + ChatColor.YELLOW + " Try /lw help.");
+					}
+				} else {
+							run.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "LW" + ChatColor.GOLD + "]" + ChatColor.YELLOW + " Error: " + ChatColor.DARK_RED + "Incorrect syntax!" + ChatColor.YELLOW + " Try /lw help.");
+				}
+			}
 		return false;
 	}
 }
